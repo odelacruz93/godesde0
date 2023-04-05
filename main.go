6688479a1d032f2ab48443b41886d505ecb864c5
odelacruzc <oscar.delacruz.cotrina@entel.pe>
2023-04-05 17:19:39 -0500

    Siguiente commit

--- main.go
+++ main.go
@@ -3,11 +3,34 @@ package main
 import (
 	"fmt"
 
-	"github.com/odelacruz93/godesde0/variables"
+	ejercicios "github.com/odelacruz93/godesde0/Ejercicios"
 )
 
 func main() {
-	estado, texto := variables.ConvierteTexto(1993)
-	fmt.Println(estado)
+	// estado, texto := variables.ConvierteTexto(1993)
+	// fmt.Println(estado)
+	// fmt.Println(texto)
+
+	// os := runtime.GOOS
+	// if os == "Linux." || os == "OS X." {
+	// 	fmt.Println("El sistema Operativo es: ", os)
+	// } else {
+	// 	fmt.Println("El sistema Operativo no es linux sino es: ", os)
+	// }
+
+	// switch os := runtime.GOOS; os {
+	// case "linux":
+	// 	fmt.Println("Esto es Linux")
+	// case "Darwin":
+	// 	fmt.Println("Esto es Darwin")
+	// case "windows":
+	// 	fmt.Println("Esto es windows")
+	// default:
+	// 	fmt.Printf("%s \n", os)
+	//}
+
+	numero, texto := ejercicios.ConvertirNumerico("6500")
+	fmt.Println(numero)
 	fmt.Println(texto)
+
 }
